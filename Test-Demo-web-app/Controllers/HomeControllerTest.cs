d804f075c7d70001610b2b1d3bf3ffbd4acd3992
ranji <ranjit.kumar05@mphasis.com>
2021-02-12 17:17:59 +0530

    Initial Commit

---
+++ Test-Demo-web-app/Controllers/HomeControllerTest.cs
@@ -0,0 +1,41 @@
+﻿
+using demo_web_app.Controllers;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+
+namespace Test_Demo_web_app
+{
+    [TestClass]
+    public class HomeControllerTest
+    {
+        public readonly ILogger<HomeController> _logger;
+
+        [TestMethod]
+        public void Index()
+        {
+            // Arrange
+            HomeController controller = new HomeController(_logger);
+
+            // Act
+            ViewResult result = controller.Index() as ViewResult;
+
+            // Assert
+            Assert.IsNotNull(result);
+        }
+
+        [TestMethod]
+        public void Privacy()
+        {
+            // Arrange
+            HomeController controller = new HomeController(_logger);
+
+            // Act
+            ViewResult result = controller.Privacy() as ViewResult;
+
+            // Assert
+            Assert.IsNotNull(result);
+        }
+    }
+}
---
+++ Test-Demo-web-app/UnitTest1.cs
@@ -0,0 +1,14 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Test_Demo_web_app
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+
+        }
+    }
+}
